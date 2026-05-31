99c8c2f73df53b0a2b618424418ae42d872fda4c
Nico Naufal Arifiyanto <niconovel21@gmail.com>
2026-05-31 11:43:49 +0700

    Fix MLproject as file

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -6,21 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-# Tracking URI
-mlflow.set_tracking_uri("http://127.0.0.1:5001")
-
-# Experiment
 mlflow.set_experiment("Stock_Prediction")
 
-# WAJIB (kriteria Basic)
 mlflow.sklearn.autolog(log_models=True)
 
-# Load dataset
 df = pd.read_csv(
-    "MLProject/dataset_preprocessing/hasil_preprocessing.csv"
+    "Workflow-CI/dataset_preprocessing/hasil_preprocessing.csv"
 )
 
-# Features & target
 X = pd.get_dummies(
     df.drop("LastPrice", axis=1),
     drop_first=True
@@ -28,7 +21,6 @@ X = pd.get_dummies(
 
 y = df["LastPrice"]
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -38,33 +30,17 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 with mlflow.start_run():
 
-    # Model
     model = RandomForestRegressor(
         n_estimators=100,
         random_state=42
     )
 
-    # Train
     model.fit(X_train, y_train)
 
-    # Predict
     y_pred = model.predict(X_test)
 
-    # Metric
     mse = mean_squared_error(y_test, y_pred)
 
-    print("MSE:", mse)
-
-    # Log metric
     mlflow.log_metric("mse", mse)
 
-    # Save model artifact
-    mlflow.sklearn.save_model(
-        sk_model=model,
-        path="saved_model"
-    )
-
-    # Log artifacts manually
-    mlflow.log_artifacts("saved_model", artifact_path="model")
-
-    print("Model artifact berhasil disimpan")
+    print(f"MSE: {mse}")
